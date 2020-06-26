51974e1d06c5eed904ea5800524113bc2489d623
Narration SD <narrationsd@gmail.com>
2020-06-26 00:20:56 -0700

    Here's pouchdb-upsert working, by pinning pouchdb-promise to 6.2.0. And thus we learn that it preserves history, not over-writing a rev.

--- src/pages/DataView.vue
+++ src/pages/DataView.vue
@@ -3,14 +3,14 @@
     <h2>Data View</h2>
     <hr>
     <div class="json-data" v-html="pouchData"></div>
-<!--    <div><h3>{{ pouchData }}</h3></div>-->
+    <!--    <div><h3>{{ pouchData }}</h3></div>-->
   </span>
 </template>
 
 <script>
 import PouchDb from 'pouchdb'
 import PouchDbFind from 'pouchdb-find'
-// import PouchDbUpsert from 'pouchdb-upsert'
+import PouchDbUpsert from 'pouchdb-upsert'
 import beautify from 'json-beautify'
 
 export default {
@@ -24,92 +24,53 @@ export default {
   created: function () {
     this.pouchData = 'Searching...'
     PouchDb.plugin(PouchDbFind)
-    // PouchDb.plugin(PouchDbUpsert)
+    PouchDb.plugin(PouchDbUpsert)
     const dbName = 'hard-begin'
 
     this.db = new PouchDb(dbName)
-      // .then(result => console.log(result))
-      // .catch(err => console.log(err))
+    // .then(result => console.log(result))
+    // .catch(err => console.log(err))
     this.db.info().then(function (info) {
       console.log('info: ' + JSON.stringify(info))
     })
-    // this.db.remove({
-    //     _id: 'mydoc'
-    // })
-    //   .then(result => {
-    //     console.log(result)
-    //   })
-    //   .catch(err => {console.log(err)})
-
-    // this way to only update a single, non-time-sequenced element, as we use
-    // we've really done an upsert here, so function coming
-    this.db.find({
-      selector: {
-        title: 'Roma'
-      }
-    }).then(result => {
-      const doc = result.docs[0]
-      console.log('doc: ' + JSON.stringify(doc))
-      this.db.put({
-        _id: doc._id,
-        _rev: doc._rev,
-        title: 'Roma',
-        description: 'A great true film of Mexico, from accurate lives, in not quite recent times.'
-      }).then(result => {
-        console.log('put: ' + JSON.stringify(result))
-        return this.db.createIndex({
-          index: {fields: ['title']}
+    // we've really done an upsert of a kind here, it turns out preserving history
+    this.db.upsert('wut',
+      function (doc) {
+        Object.assign(doc, {
+          _id: 'wut',
+          title: 'Roma',
+          description: 'A truly great true film of Mexico, from accurate lives, in not quite recent times.'
         })
-      }).then(result => {
-        console.log('index: ' + JSON.stringify(result))
-      }).catch(function (err) {
-        console.log('put err: ' + JSON.stringify(err))
+
+        if (!doc.count) {
+          doc.count = 0;
+        }
+        doc.count++;
+        console.log('upsert: ' + JSON.stringify(doc))
+        return doc;
+    }).then(doc => {
+      console.log('upsert: ' + JSON.stringify(doc))
+      return this.db.createIndex({
+        index: {fields: ['title']}
       })
-    }).catch(function (err) {
-      console.log('get err, so create: ' + JSON.stringify(err))
-      // this.db.put({
-      //   _id: 'mydo01',
-      //   title: 'Roma',
-      //   description: 'A great true film of Mexico, from accurate lives, in not quite recent times.'
-      // }).then(result => {
-      //   console.log('put new: ' + JSON.stringify(result))
-      // }).catch(function (err) {
-      //   console.log('put new err: ' + JSON.stringify(err))
-      // })
-    })
-
-    // this.db.createIndex({
-    //   index: {fields: ['title']}
-    // }).then(result => {
-    //   console.log('index: ' + JSON.stringify(result))
-    // }).catch(function (err) {
-    //   console.log('index new err: ' + JSON.stringify(err))
-    // })
-
-      // remember this is a promise, thus call back sets data when ready
-      this.db.find ({
+    }).then(result => {
+      console.log('index: ' + JSON.stringify(result))
+      this.db.find({
         selector: {
           title: 'Roma'
         }
-      }).then (result => {
-          const data =
-            '<pre>\n' +
-            // JSON.stringify(result, null, 2)
-            beautify(result, null, 2, 80)
-            + '\n</pre>'
-          console.log(data)
-          this.pouchData = data
-        })
-        .catch(err => {
-          console.log('find: ' + JSON.stringify(err))
-        })
-    // }).catch(err => {
-    //   console.log('index err: ' + JSON.stringify(err))
-    // }).finally(() =>{
-    //   // this.db.destroy(dbName)
-    //   //   .then (result => console.log('destroy: ' + JSON.stringify(result)))
-    //   //   .catch(err => console.log('destroy: ' + JSON.stringify(err)))
-    // })
+      }).then(result => {
+        const data =
+          '<pre>\n' +
+          // JSON.stringify(result, null, 2)
+          beautify(result, null, 2, 80)
+          + '\n</pre>'
+        console.log(data)
+        this.pouchData = data
+      })
+    }).catch(err => {
+      console.log('upsert error: ' + JSON.stringify(err))
+    })
   }
 }
 </script>
