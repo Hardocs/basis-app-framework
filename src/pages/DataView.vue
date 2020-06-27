ec02e16954b6d6f983a39f30d40fa969ea45facd
Narration SD <narrationsd@gmail.com>
2020-06-26 19:06:41 -0700

    this is discovery, ending up with responsive-capable, double-byte-safe wrap for raw JSON data -- handy. Keeping the trail in case, but nice to have both.

--- src/pages/DataView.vue
+++ src/pages/DataView.vue
@@ -11,7 +11,8 @@
 import PouchDb from 'pouchdb'
 import PouchDbFind from 'pouchdb-find'
 import PouchDbUpsert from 'pouchdb-upsert'
-import beautify from 'json-beautify'
+import WrapAnsi from 'wrap-ansi'
+import Smartwrap from 'smartwrap'
 
 export default {
   name: "DataView",
@@ -41,7 +42,7 @@ export default {
         Object.assign(doc, {
           _id: 'wut',
           title: 'Roma',
-          description: 'A great true film of Mexico, from accurate lives, in not quite recent times.'
+          description: 'A great true film  宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符of Mexico, from accurate lives, in not quite recent times.'
         })
 
         if (!doc.count) {
@@ -62,17 +63,43 @@ export default {
           title: 'Roma'
         }
       }).then(result => {
-        const data =
-          '<pre>\n' +
-          // JSON.stringify(result, null, 2)
-          beautify(result, null, 2, 80)
-          + '\n</pre>'
+        console.log(result)
+        // const xresult = 'just one line 宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符 but a long enough one to see if that is really the problem we see this time'
+        // const xresult = 'just one line but a long enough one to see if that is really the problem we see this time'
+        let data = WrapAnsi(JSON.stringify(result, null, 2),
+          80,
+          { trim: false, hard: true})
+          // data = JSON.stringify(result, null, 2)
+          // *todo* wrap ourselves, to suit pre
+        // data = Smartwrap(JSON.stringify(result, null, 2), { width: 40,  hard: true })
+        // data = this.multiWrap(JSON.stringify(result, null, 2))
+        // set width by viewport, will be something like 30 for small phone, to 80 or above laptop
+        data = this.responsiveWrap(JSON.stringify(result, null, 2), 30)
+        console.log(typeof result)
+        console.log (JSON.stringify(result))
+        console.log (JSON.stringify(result.docs[0]))
+
+
         console.log(data)
-        this.pouchData = data
+        this.pouchData = '<pre>\n' + data + '\n</pre>'
       })
     }).catch(err => {
       console.log('upsert error: ' + JSON.stringify(err))
     })
+  },
+  methods: {
+    responsiveWrap: (text, width) => {
+      return WrapAnsi(text, width, { trim: false, hard: true})
+    },
+    multiWrap: (paragraph) => {
+      const lines = paragraph.split('\n')
+      console.log ('lines: ' + JSON.stringify(lines))
+      const outLines = lines.map (line => {
+        return Smartwrap(line, { width: 40, trim: false, hard: true })
+      })
+      console.log ('outLines: ' + JSON.stringify(outLines))
+      return outLines.join('\n')
+    }
   }
 }
 </script>
@@ -80,6 +107,7 @@ export default {
 <style scoped>
 .json-data {
   text-align: left;
+  white-space: normal;
   margin: 20px;
 }
 </style>
