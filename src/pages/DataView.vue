244c460dc199e9ec6dcbb06e9b2c1f15dd3c2928
Narration SD <narrationsd@gmail.com>
2020-06-26 19:27:28 -0700

    cleaned up for use, and a few comments forwards

--- src/pages/DataView.vue
+++ src/pages/DataView.vue
@@ -12,7 +12,6 @@ import PouchDb from 'pouchdb'
 import PouchDbFind from 'pouchdb-find'
 import PouchDbUpsert from 'pouchdb-upsert'
 import WrapAnsi from 'wrap-ansi'
-import Smartwrap from 'smartwrap'
 
 export default {
   name: "DataView",
@@ -31,20 +30,26 @@ export default {
     this.db = new PouchDb(dbName, {
       revs_limit: 1.
     })
-    // .then(result => console.log(result))
-    // .catch(err => console.log(err))
+
+    // no promise, so we'll check the status here
     this.db.info().then(function (info) {
       console.log('info: ' + JSON.stringify(info))
     })
+
     // we've really done an upsert of a kind here, it turns out preserving history
     this.db.upsert('wut',
       function (doc) {
         Object.assign(doc, {
           _id: 'wut',
           title: 'Roma',
-          description: 'A great true film  宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符of Mexico, from accurate lives, in not quite recent times.'
+          description: 'A great true film  宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽' +
+            '字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符 of Mexico, from accurate ' +
+            'lives, in not quite recent times.'
         })
 
+        // this count thing isn't needed, but useful from example, and if we
+        // should want to keep track of how many updates a data has had. Reve,
+        // though, are the thing.
         if (!doc.count) {
           doc.count = 0;
         }
@@ -63,24 +68,15 @@ export default {
           title: 'Roma'
         }
       }).then(result => {
-        console.log(result)
-        // const xresult = 'just one line 宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符宽字符 but a long enough one to see if that is really the problem we see this time'
-        // const xresult = 'just one line but a long enough one to see if that is really the problem we see this time'
-        let data = WrapAnsi(JSON.stringify(result, null, 2),
-          80,
-          { trim: false, hard: true})
-          // data = JSON.stringify(result, null, 2)
-          // *todo* wrap ourselves, to suit pre
-        // data = Smartwrap(JSON.stringify(result, null, 2), { width: 40,  hard: true })
-        // data = this.multiWrap(JSON.stringify(result, null, 2))
-        // set width by viewport, will be something like 30 for small phone, to 80 or above laptop
-        data = this.responsiveWrap(JSON.stringify(result, null, 2), 30)
-        console.log(typeof result)
-        console.log (JSON.stringify(result))
-        console.log (JSON.stringify(result.docs[0]))
 
-
-        console.log(data)
+        // this form of stringify gives nice indented lines,
+        // but we still have to wrap the long ones, since
+        // we're presenting in <pre>
+        // *todo* later, hook up the wrap width to screen viewport
+        //  width, for the full effect.
+        const data = this.responsiveWrap(
+            JSON.stringify(result, null, 2),
+          80)
         this.pouchData = '<pre>\n' + data + '\n</pre>'
       })
     }).catch(err => {
@@ -90,15 +86,6 @@ export default {
   methods: {
     responsiveWrap: (text, width) => {
       return WrapAnsi(text, width, { trim: false, hard: true})
-    },
-    multiWrap: (paragraph) => {
-      const lines = paragraph.split('\n')
-      console.log ('lines: ' + JSON.stringify(lines))
-      const outLines = lines.map (line => {
-        return Smartwrap(line, { width: 40, trim: false, hard: true })
-      })
-      console.log ('outLines: ' + JSON.stringify(outLines))
-      return outLines.join('\n')
     }
   }
 }
