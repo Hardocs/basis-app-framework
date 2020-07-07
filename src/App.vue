68f67760c032534f660a8f64ed514c5115607a39
Narration SD <narrationsd@gmail.com>
2020-07-07 16:40:25 -0700

    a better off-white later, if can be found

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: lightgoldenrodyellow;
+  background-color: white;
   min-height: 100vh;
 }
 </style>
