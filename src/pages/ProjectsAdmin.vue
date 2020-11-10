efd864944e0981af08b4f45e588f3374e71aea97
Narration SD <narrationsd@gmail.com>
2020-11-10 14:52:39 -0800

    spelling and version

--- src/pages/ProjectsAdmin.vue
+++ src/pages/ProjectsAdmin.vue
@@ -122,7 +122,7 @@
             </button>
             <button @click="replicateTestLocation" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
               focus:outline-none focus:shadow-outline" type="button">
-              Replicate Teat Location
+              Replicate Test Location
             </button>
           </div>
         </form>
